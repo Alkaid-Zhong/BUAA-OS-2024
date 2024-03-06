f32e9e563268983748bf4bb6762983d040970e64
钟昊翔 <21231025@buaa.edu.cn>
2024-03-06 21:48:30 +0800

    2

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -2,4 +2,4 @@
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
 
-cat $1 | grep -n "$2" | awk -F: '{print $1}' > result
+cat $1 | grep -n "$2" | awk -F: '{print $1}' > $3
