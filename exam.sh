f48f0d6602658393bffe2cd516c31b819f1cc4fc
钟昊翔 <21231025@buaa.edu.cn>
2024-03-13 19:42:42 +0800

    1

---
+++ exam.sh
@@ -0,0 +1,14 @@
+mkdir test
+cp -r ./code ./test
+cat ./code/14.c
+a=1
+while [ $a -lt 16 ]
+do
+	gcc -c ./test/code/$a.c -o ./test/code/$a.o
+	a=$[$a+1]
+done
+gcc ./test/code/*.o -o ./test/hello
+./test/hello 2> ./test/err.txt
+mv ./test/err.txt ./
+chmod -rw -r -xr -x  err.txt
+
