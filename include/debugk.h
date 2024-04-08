09d7389d2618c498b0a3e2c8f4f7ba5b8b7dd7e3
钟昊翔 <21231025@buaa.edu.cn>
2024-04-08 06:45:00 +0000

    modified debugk

--- include/debugk.h
+++ include/debugk.h
@@ -1,11 +1,11 @@
 #ifndef _DBGK_H_
 #define _DBGK_H_
-#include <printk.h>
-#define DEBUGK // 可注释
+#define DEBUGK
 
 #ifdef DEBUGK
 #define DEBUG_OUTPUT 1
 #else
 #define DEBUG_OUTPUT 0
 #endif
+
 #endif // !_DBGK_H_
