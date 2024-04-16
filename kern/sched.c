f4e5ffc3b244eded9c45b803fd37e8cac3317d48
钟昊翔 <21231025@buaa.edu.cn>
2024-04-16 06:59:16 +0000

    lab3: exercise3.12 fix env_sched_link

--- kern/sched.c
+++ kern/sched.c
@@ -38,8 +38,8 @@ void schedule(int yield) {
 	count--;
 	if ((yield != 0) || (count == 0) || (e == NULL) || (e->env_status != ENV_RUNNABLE)) {
 		if (e->env_status == ENV_RUNNABLE) {
-			TAILQ_REMOVE(&env_sched_link, e, env_link);
-			TAILQ_INSERT_TAIL(&env_sched_link, e, env_link);
+			TAILQ_REMOVE(&env_sched_link, e, env_sched_link);
+			TAILQ_INSERT_TAIL(&env_sched_link, e, env_sched_link);
 		}
 		panic_on(TAILQ_EMPTY(&env_sched_list));
 		e = TAILQ_FIRST(&env_sched_list);
