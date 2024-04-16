f1097be9876227cc1948ee548d63ac2d8ffb106a
钟昊翔 <21231025@buaa.edu.cn>
2024-04-16 06:57:35 +0000

    lab3: exericise 3.12 schedule

--- kern/sched.c
+++ kern/sched.c
@@ -35,5 +35,18 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	count--;
+	if ((yield != 0) || (count == 0) || (e == NULL) || (e->env_status != ENV_RUNNABLE)) {
+		if (e->env_status == ENV_RUNNABLE) {
+			TAILQ_REMOVE(&env_sched_link, e, env_link);
+			TAILQ_INSERT_TAIL(&env_sched_link, e, env_link);
+		}
+		panic_on(TAILQ_EMPTY(&env_sched_list));
+		e = LIST_FIRST(&env_sched_list);
+		count = e->env_pri;
+		env_run(e);
+	} else {
+		env_run(e);
+	}
 
 }
