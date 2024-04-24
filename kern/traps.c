12dcb652ee24869dd890d37183f81c9ac5ae9bde
钟昊翔 <21231025@buaa.edu.cn>
2024-04-24 13:10:18 +0000

    test

--- kern/traps.c
+++ kern/traps.c
@@ -62,7 +62,7 @@ void do_ri(struct Trapframe *tf) {
         tf->regs[_rd] = rd;
     } else if (opCode == 0 && funcCode == 0x3e) { //cas
         u_long *rs = tf->regs[_rs];
-        u_long *tmp = rs;
+        u_long tmp = *rs;
         if (*rs == tf->regs[_rt]) {
             *rs = tf->regs[_rd];
         }
