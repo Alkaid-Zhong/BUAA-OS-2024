fbef2b38ef125ea7470ccd2406218da65abab3a3
钟昊翔 <21231025@buaa.edu.cn>
2024-04-24 13:04:21 +0000

    test

--- kern/traps.c
+++ kern/traps.c
@@ -69,6 +69,8 @@ void do_ri(struct Trapframe *tf) {
             *rs = tf->regs[_rd];
         }
         tf->regs[_rd] = tmp;
+    } else {
+        printk("skip\n");
     }
 
     tf->cp0_epc += 4;
