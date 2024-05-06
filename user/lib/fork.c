1723134101fc5fffed263606758c2376c8064760
钟昊翔 <21231025@buaa.edu.cn>
2024-05-06 08:13:12 +0000

    lab4: exercise4.10 duppage

--- user/lib/fork.c
+++ user/lib/fork.c
@@ -72,12 +72,25 @@ static void duppage(u_int envid, u_int vpn) {
 	/* Step 1: Get the permission of the page. */
 	/* Hint: Use 'vpt' to find the page table entry. */
 	/* Exercise 4.10: Your code here. (1/2) */
+	addr = vpn * PAGE_SIZE;
+	perm = vpt[vpn] & 0xfff;
 
 	/* Step 2: If the page is writable, and not shared with children, and not marked as COW yet,
 	 * then map it as copy-on-write, both in the parent (0) and the child (envid). */
 	/* Hint: The page should be first mapped to the child before remapped in the parent. (Why?)
 	 */
 	/* Exercise 4.10: Your code here. (2/2) */
+	int flag = 0;
+	if ((perm & PTE_D) && !(perm & PTE_LIBRARY)) {
+		perm = (perm & (~PTE_D)) | PTE_COW;
+		flag = 1;
+	}
+
+	syscall_mem_map(0, addr, envid, addr, perm);
+
+	if (flag) {
+		syscall_mem_map(0, addr, 0, addr, perm);
+	}
 
 }
 
