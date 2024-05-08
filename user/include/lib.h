0f4f686e66ebb59dc45bf27fceeee10d5d22912c
钟昊翔 <21231025@buaa.edu.cn>
2024-05-08 11:05:38 +0000

    lab4-1-exam lib.h

--- user/include/lib.h
+++ user/include/lib.h
@@ -73,6 +73,12 @@ int syscall_read_dev(void *va, u_int dev, u_int len);
 void ipc_send(u_int whom, u_int val, const void *srcva, u_int perm);
 u_int ipc_recv(u_int *whom, void *dstva, u_int *perm);
 
+// lab4-1-exam
+void sem_open(int sem_id, int n);
+int sem_wait(int sem_id);
+int sem_post(int sem_id);
+int sem_kill(int sem_id);
+
 // wait.c
 void wait(u_int envid);
 
