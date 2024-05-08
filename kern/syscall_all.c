00736e31690602cb7bc8e6e8e7d5e98fad1debe8
钟昊翔 <21231025@buaa.edu.cn>
2024-05-08 12:31:24 +0000

    fix status

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -579,13 +579,12 @@ int sys_msg_status(u_int msgid) {
 	struct Msg *m;
 
 	/* Your Code Here (3/3) */
-	TAILQ_FOREACH(m, &msg_free_list, msg_link) {
-		if (msg2id(m) == msgid) {
-			return m->msg_status;
-		}
-		if (msg2id(m) > msgid) {
-			return MSG_RECV;
-		}
+	m = msgs[MSGX(msgid)];
+	if (msg2id(m) == msgid) {
+		return m->msg_status;
+	}
+	if (msg2id(m) > msgid) {
+		return MSG_RECV;
 	}
 	return -E_INVAL;
 }
