46978df455b129ffb79173c62e13028b5765c79f
钟昊翔 <21231025@buaa.edu.cn>
2024-05-22 11:47:00 +0000

    test env

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -19,13 +19,13 @@ void strace_send(int sysno) {
 
 	// Your code here. (1/2)
 	u_int cur_env_id = syscall_getenvid();
-	struct Env *curenv = &envs[ENVX(cur_env_id)];
+	struct Env cur_env = envs[ENVX(cur_env_id)];
 
 	if (straced != 0) {
 		int r = straced;
 		straced = 0;
-		syscall_ipc_try_send(curenv->env_parent_id, sysno, 0, 0);
-		syscall_set_env_status(curenv->env_id, ENV_NOT_RUNNABLE);
+		syscall_ipc_try_send(cur_env.env_parent_id, sysno, 0, 0);
+		syscall_set_env_status(cur_env.env_id, ENV_NOT_RUNNABLE);
 		straced = r;
 	}
 }
@@ -33,15 +33,15 @@ void strace_send(int sysno) {
 void strace_recv() {
 	// Your code here. (2/2)
 	u_int cur_env_id = syscall_getenvid();
-	struct Env *curenv = &envs[ENVX(cur_env_id)];
+	struct Env cur_env = envs[ENVX(cur_env_id)];
 
 	while(1) {
 		syscall_ipc_recv(0);
-		u_int sysno = curenv->env_ipc_value;
+		u_int sysno = cur_env.env_ipc_value;
 		if (sysno == SYS_env_destroy) {
 			break;
 		}
-		u_int child_env_id = curenv->env_ipc_from;
+		u_int child_env_id = cur_env.env_ipc_from;
 		strace_barrier(child_env_id);
 		recv_sysno(child_env_id, sysno);
 		syscall_set_env_status(child_env_id, ENV_RUNNABLE);
