2fbfd05fe68343df649c387752bce248fe8e98d4
钟昊翔 <21231025@buaa.edu.cn>
2024-05-22 12:11:37 +0000

    test

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -24,27 +24,27 @@ void strace_send(int sysno) {
 		straced = 0;
 		u_int cur_env_id = syscall_getenvid();
 		struct Env cur_env = envs[ENVX(cur_env_id)];
-		syscall_set_env_status(0, ENV_NOT_RUNNABLE);
 		syscall_ipc_try_send(cur_env.env_parent_id, sysno, 0, 0);
+		syscall_set_env_status(cur_env.env_id, ENV_NOT_RUNNABLE);
 		straced = r;
 	}
 }
 
 void strace_recv() {
 	// Your code here. (2/2)
-	u_int cur_env_id = syscall_getenvid();
-	struct Env cur_env = envs[ENVX(cur_env_id)];
 
 	while(1) {
+		u_int cur_env_id = syscall_getenvid();
+		struct Env cur_env = envs[ENVX(cur_env_id)];
 		syscall_ipc_recv(0);
 		u_int sysno = cur_env.env_ipc_value;
-		if (sysno == SYS_env_destroy) {
-			break;
-		}
 		u_int child_env_id = cur_env.env_ipc_from;
 		strace_barrier(child_env_id);
 		recv_sysno(child_env_id, sysno);
 		syscall_set_env_status(child_env_id, ENV_RUNNABLE);
+		if (sysno == SYS_env_destroy) {
+			break;
+		}
 	}
 
 }
