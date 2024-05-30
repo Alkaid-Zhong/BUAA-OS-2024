3c046b3ab91c202ab0d1a0fdcc683e10958653a9
钟昊翔 <21231025@buaa.edu.cn>
2024-05-30 03:51:31 +0000

    test

--- tests/lab5_2/fs_check.c
+++ tests/lab5_2/fs_check.c
@@ -87,36 +87,50 @@ void fs_check() {
 
 int main() {
 	fs_init();
-	//fs_check();
-	struct File *f;
 	int r;
-	debugf("test\n");
-	if ((r = open("/newmotd", O_RDWR)) < 0) {
-		user_panic("file_open /newmotd: %d", r);
-	}
-	debugf("fdnum: %d\n", r);
-
+	int fdnum;
 	char buf[512];
-	int pid;
-	int fdnum = r;
-	if ((pid = fork()) == 0) {
-		debugf("child process id: %d\n", syscall_getenvid());
-		if ((r = read(fdnum, buf, 511)) < 0) {
+	int n;
+
+	if ((r = open("/newmotd", O_RDWR)) < 0) {
+		user_panic("open /newmotd: %d", r);
+	}
+	fdnum = r;
+	debugf("open is good\n");
+
+	if ((n = read(fdnum, buf, 511)) < 0) {
+		user_panic("read /newmotd: %d", r);
+	}
+	if (strcmp(buf, diff_msg) != 0) {
+		user_panic("read returned wrong data");
+	}
+	debugf("read is good\n");
+
+	int id;
+
+	if ((id = fork()) == 0) {
+		if ((n = read(fdnum, buf, 511)) < 0) {
 			user_panic("child read /newmotd: %d", r);
 		}
-		debugf("child r after read: %d\n", r);
-		struct Fd *fdd;
-		fd_lookup(r, &fdd);
-		debugf("child fd->offset: %d\n", fdd->fd_offset);
-	} else {
-		debugf("parent process id: %d\n", syscall_getenvid());
-		if ((r = read(fdnum, buf, 511)) < 0) {
-			user_panic("parent read /newmotd: %d", r);
+		if (strcmp(buf, diff_msg) != 0) {
+			user_panic("child read returned wrong data");
 		}
-		debugf("parent r after read: %d\n", r);
+		debugf("child read is good && child_fd == %d\n",r);
 		struct Fd *fdd;
-		fd_lookup(r, &fdd);
-		debugf("parent fd->offset: %d\n", fdd->fd_offset);
+		fd_lookup(r,&fdd);
+		debugf("child_fd's offset == %d\n",fdd->fd_offset);
+	}
+	else {
+		if((n = read(fdnum, buf, 511)) < 0) {
+			user_panic("father read /newmotd: %d", r);
+		}
+		if (strcmp(buf, diff_msg) != 0) {
+			user_panic("father read returned wrong data");
+		}
+		debugf("father read is good && father_fd == %d\n",r);
+		struct Fd *fdd;
+		fd_lookup(r,&fdd);
+		debugf("father_fd's offset == %d\n",fdd->fd_offset);
 	}
  	return 0;
 }
