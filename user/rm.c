a29f54d9cec5e6e504faa8d840cbde7dbbd17497
钟昊翔 <21231025@buaa.edu.cn>
2024-06-13 10:22:06 +0000

    fix rm typo

--- user/rm.c
+++ user/rm.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
         remove(path);
     } else {
         if (!r) {
-            printf("rm: cannot remove '%s': Is a directory\n", path)
+            printf("rm: cannot remove '%s': Is a directory\n", path);
             return 1;
         }
         remove(path);
