257739f2b5f5999650bd62b1ba9c190b6ae65a42
钟昊翔 <21231025@buaa.edu.cn>
2024-06-13 08:11:49 +0000

    fix

--- user/sh.c
+++ user/sh.c
@@ -244,7 +244,7 @@ void runcmd_conditional(char *s) {
 			user_panic("fork: %d", r);
 		}
 		if (r == 0) {
-			exit_status = runcmd(buf);
+			exit_status = runcmd(cmd_buf);
 			syscall_ipc_try_send(env->env_parent_id, exit_status, 0, 0);
 			exit();
 		} else {
