d0bb4dbf68927d980785953a2453b08981500e44
钟昊翔 <21231025@buaa.edu.cn>
2024-06-14 02:59:28 +0000

    test

--- user/sh.c
+++ user/sh.c
@@ -99,8 +99,6 @@ int parsecmd(char **argv, int *rightpipe) {
 			dup(fd, 0);
 			close(fd);
 
-			//user_panic("< redirection not implemented");
-
 			break;
 		case '>':
 			if (gettoken(0, &t) != 'w') {
@@ -120,8 +118,6 @@ int parsecmd(char **argv, int *rightpipe) {
 			}
 			dup(fd, 1);
 			close(fd);
-
-			//user_panic("> redirection not implemented");
 
 			break;
 		case '|':;
@@ -207,7 +203,7 @@ int runcmd(char *s) {
 	if (rightpipe) {
 		wait(rightpipe);
 	}
-	debugf("command %s exit with return value %d\n", argv[0], exit_status);
+	// debugf("command %s exit with return value %d\n", argv[0], exit_status);
 	return exit_status;
 	// exit();
 }
@@ -252,22 +248,13 @@ void runcmd_conditional(char *s) {
 				user_panic("fork: %d", r);
 			}
 			if (r == 0) {
-				syscall_ipc_try_send(env->env_parent_id, 2024, 0, 0);
-				syscall_ipc_recv(0);
 				exit_status = runcmd(cmd_buf);
 				syscall_ipc_try_send(env->env_parent_id, exit_status, 0, 0);
 				exit();
 			} else {
 				syscall_ipc_recv(0);
-				int magic_num = env->env_ipc_value;
-				if (magic_num != 2024) {
-					exit_status = 2024;
-				} else {
-					syscall_ipc_try_send(r, 2024, 0, 0);
-					syscall_ipc_recv(0);
-					exit_status = env->env_ipc_value;
-				}
 				wait(r);
+				exit_status = env->env_ipc_value;
 				// debugf("command %s and op %c exit with return value %d\n", cmd_buf, op, exit_status);
 			}
 
