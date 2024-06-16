27bec7ce4a1fddb3669f7dbce5adcfd2a7e5d61c
钟昊翔 <21231025@buaa.edu.cn>
2024-06-16 20:20:12 +0800

    test

--- user/sh.c
+++ user/sh.c
@@ -437,7 +437,7 @@ int runcmd(char *s, int background_exc) {
 			user_panic("fg: (0x%08x) not running\n", job_id);
 		}
 		if (jobs[job_id].status == 0) {
-			debugf("kill job (%d) (0x%08x): %d\n", jobs[i].job_id, jobs[i].pid, syscall_env_destroy(jobs[i].pid));
+			debugf("kill job (%d) (0x%08x): %d\n", job_id, jobs[job_id].pid, syscall_env_destroy(jobs[job_id].pid));
 		} else {
 			user_panic("fg: job (%d) do not exist\n", job_id);
 		}
